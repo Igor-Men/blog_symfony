c30817b3180a96128f834d8a4ebaebf0f6de57be
Igor-Men <dp030988kie@gmail.com>
2016-11-17 23:37:20 +0200

    create in controller query to entyty

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Post;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,8 +14,31 @@ class BlogController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        /*// replace this example code with whatever you need
+        $blog = new Post();
+        $blog->setTitle('firstOne');
+        $blog->setText('some text for article');
 
-        return $this->render('blog/index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        // tells Doctrine you want to (eventually) save the Product (no queries yet)
+        $em->persist($blog);
+        // actually executes the queries (i.e. the INSERT query)
+        $em->flush();
+
+        return new Response('Saved new product with id '.$product->getId());*/
+
+        $posts = $this->getDoctrine()
+            ->getRepository('AppBundle:Post')
+            ->findAll();
+
+        $posts_arr = [];
+        foreach ($posts as $post) {
+            $posts_arr[] = [
+                'id' => $post->getId(),
+                'title' => $post->getTitle(),
+                'text' => $post->getText(),
+            ];
+        }
+        return $this->render('blog/index.html.twig', ["posts" => $posts_arr]);
     }
 }
